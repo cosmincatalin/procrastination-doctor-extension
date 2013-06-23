e0b080ef3ae27c1d1317cb46edf1d5d4fc191767
Cosmin Catalin Sanda <cosmincatalin@gmail.com>
2013-06-23 16:15:36 +0200

    This object is used to attach the handlers

---
+++ js/backend/monitor.js
@@ -0,0 +1,27 @@
+Mooment.monitor = (function() {
+  function start(callback) {
+    chrome.tabs.onUpdated.addListener( function(tabId, changeInfo, tab ) {
+      // Only start monitoring once the page is completely loaded
+      if (changeInfo.status !== "complete" ) {
+        return;
+      }
+      console.log("Tab updated");
+      try {
+        Mooment.host.setActive(Mooment.util.getSite(tab.url));
+      // Silently ignore if the url fails to be parsed
+      } catch (ex) { }
+    });
+    chrome.tabs.onActivated.addListener(function(activeInfo) {
+      chrome.tabs.get(activeInfo.tabId, function(tab) {
+        try{
+          Mooment.host.setActive(Mooment.util.getSite(tab.url));
+        // Silently ignore if the url fails to be parsed
+        } catch (ex) { }
+      });
+    });
+  }
+
+  return {
+    start: start
+  };
+}());
