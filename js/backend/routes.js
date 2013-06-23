afb757d50c1b6066910a348d63085794f5a93d57
Cosmin Catalin Sanda <cosmincatalin@gmail.com>
2013-06-23 16:17:02 +0200

    Routes for the backend

---
+++ js/backend/routes.js
@@ -0,0 +1,43 @@
+// Defines the public routes
+chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
+
+  /**
+   * Since the callback of the onMessage handler accepts only
+   * one parameter, we have to pack the response before calling
+   * the function
+   */
+  function prepareAndSendCallbackResponse(err, response) {
+    // If there's no callback set, it makes no sense to call it
+    if( sendResponse ) {
+      sendResponse({
+        "err": err,
+        "response": response
+      });
+    }
+  }
+
+  switch (request.controller) {
+  // User Controller
+  case "user":
+    switch (request.action) {
+    case "authenticate":
+      Mooment.user.authenticate(request.params, prepareAndSendCallbackResponse);
+      break;
+    case "setToken":
+      Mooment.user.setToken(request.params, prepareAndSendCallbackResponse);
+      break;
+    }
+    break;
+  // Monitor Controller
+  case "monitor":
+    switch (request.action) {
+    case "startMonitoring":
+      Mooment.monitor.start(prepareAndSendCallbackResponse);
+      break;
+    }
+    break;
+  }
+  // This means that the extension will use the
+  // sendResponse callback to return data
+  return true;
+});
